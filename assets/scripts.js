b2be1d0fe8f650c2d482ff68e4355aec87b9573d
Michael Sefranek <michaelsefranek@Michaels-MacBook-Air.local>
2017-03-13 22:33:01 -0500

    got new cool nav working

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,14 +1,27 @@
 window.addEventListener("load",function(){
-	var navButton = document.getElementById("navclick");
+	var navButton = document.getElementById("nav-icon1");
 	var navBar = document.getElementById("navbar");
+
+
+	// navButton.addEventListener("click",function(){
+	// 	this.toggleClass('open');
+	// });
+
+	// $(document).ready(function() {
+	// 	$('#nav-icon1').click(function() {
+	// 		$(this).toggleClass('open');
+	// 	});
+	// });
 
 	var navOpen = false; //placeholder of nav state, init as false
 
 	navButton.addEventListener("click",function(){
 		if (navOpen == false){
 			navBar.style.top = "89px";
+			this.classList.add('open')
 			navOpen = true;
 		} else {
+			this.classList.remove('open')
 			navBar.style.top = "0px";
 			navOpen = false;			
 		}
@@ -16,3 +29,4 @@ window.addEventListener("load",function(){
 	});
 
 });
+
