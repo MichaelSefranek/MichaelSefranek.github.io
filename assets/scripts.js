2f9436d082142216f7d4c9db391840a21624fb13
Michael Sefranek <msefran@gmail.com>
2017-03-16 15:26:17 -0500

    fine tuning styling, added new font to header, drying up header in the middle of

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,16 +1,18 @@
 window.addEventListener("load",function(){
 	var navButton = document.getElementById("nav-icon1");
 	var navBar = document.getElementById("navbar");
+	var header = document.getElementById("header");
+	var header_height_str = header.offsetHeight + "px"
 
 	var navOpen = false; //placeholder of nav state, init as false
 
 	navButton.addEventListener("click",function(){
 		if (navOpen == false){
-			navBar.style.top = "94px";
-			this.classList.add('open')
+			navBar.style.top = header_height_str;
+			this.classList.add('open');
 			navOpen = true;
 		} else {
-			this.classList.remove('open')
+			this.classList.remove('open');
 			navBar.style.top = "0px";
 			navOpen = false;			
 		}
