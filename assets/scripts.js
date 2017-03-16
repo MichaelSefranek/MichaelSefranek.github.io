ae7636317575252a40e1a967805801eeac3fbd03
Michael Sefranek <msefran@gmail.com>
2017-03-15 22:13:00 -0500

    somehow jekyll reverted

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,17 +2,6 @@ window.addEventListener("load",function(){
 	var navButton = document.getElementById("nav-icon1");
 	var navBar = document.getElementById("navbar");
 
-
-	// navButton.addEventListener("click",function(){
-	// 	this.toggleClass('open');
-	// });
-
-	// $(document).ready(function() {
-	// 	$('#nav-icon1').click(function() {
-	// 		$(this).toggleClass('open');
-	// 	});
-	// });
-
 	var navOpen = false; //placeholder of nav state, init as false
 
 	navButton.addEventListener("click",function(){
