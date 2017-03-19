e447d95e6c12a4a426ccb9d025cd3b23b3a05541
Michael Sefranek <msefran@gmail.com>
2017-03-18 22:58:02 -0500

    added new nav and styling

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,23 @@
 window.addEventListener("load",function(){
 	var navButton = document.getElementById("nav-icon1");
-	var navBar = document.getElementById("navbar");
 	var header = document.getElementById("header");
+	var nav = document.getElementById("nav-container");
 	var header_height_str = header.offsetHeight + "px"
+
 
 	var navOpen = false; //placeholder of nav state, init as false
 
 	navButton.addEventListener("click",function(){
 		if (navOpen == false){
-			navBar.style.top = header_height_str;
+			nav.style.left = "-225px";
 			this.classList.add('open');
 			navOpen = true;
 		} else {
+			nav.style.left = "9999px";
 			this.classList.remove('open');
-			navBar.style.top = "0px";
 			navOpen = false;			
 		}
 
 	});
 
 });
-
